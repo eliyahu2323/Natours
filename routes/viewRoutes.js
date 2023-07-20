09e4e5812d018783a3c734b4cd9cdb0f2b2629c8
Eliyahu Abraham <eliavraham1919@gmail.com>
2023-07-20 10:53:51 +0300

    first commit

---
+++ routes/viewRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const viewController = require('../controllers/viewController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.get('/', authController.isLoggeIn, viewController.getOverview);
+router.get('/tour/:slug', authController.isLoggeIn, viewController.getTour);
+router.get('/login', authController.isLoggeIn, viewController.getLoginForm);
+router.get('/me', authController.protect, viewController.getAccount);
+
+router.post('/submit-user-data', viewController.updateUserData);
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log(`UNCAUGHT EXCEPTION💥 Shuting down...`);
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection seccessful'));
+
+const port = process.env.PORT || 80;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log(`UNHADLER REJECTION 💥 Shuting down...`);
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+// console.log(x);
